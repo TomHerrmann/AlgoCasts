7e46506069de48570370038055e9be1ea30dede9
Tom Herrmann <tomherrmanndev@gmail.com>
2020-01-11 19:23:42 -0500

    Solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let result;
+  let resultCount = 0;
+  const cache = {};
+
+  for (let i = 0; i < str.length; i++) {
+    cache[str[i]] = cache[str[i]] + 1 || 1;
+  }
+
+  for (let key in cache) {
+    if (!result) {
+      result = key;
+      resultCount = cache[key];
+    }
+    else {
+      if (cache[key] > resultCount) {
+        result = key;
+        resultCount = cache[key];
+      }
+    }
+  }
+
+  return result;
+}
 
 module.exports = maxChar;
