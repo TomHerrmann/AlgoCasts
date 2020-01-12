d345844dd8afac925823636b6d295d29628081ef
Tom Herrmann <tomherrmanndev@gmail.com>
2020-01-11 19:17:44 -0500

    Cleanup comments and console.logs

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -18,33 +18,6 @@ function reverseInt(n) {
 
   if (n < 0) return Number('-' + reversedIntStr);
   else return Number(reversedIntStr);
-
-  // // pre loop setup to handle negative inputs
-  // let isNegative = false;
-  // let i = 0;
-  // if (nStr[0] === '-') {
-  //   isNegative = true;
-  //   i = 1;
-  // } 
-
-  // for (i; i < nStr.length; i++) {
-  //   if (reversedIntStr.length !== '0') reversedIntStr = nStr[i] + reversedIntStr;
-  //   else {
-  //     if (nStr[i] !== '0' && nStr[i] !== '-') reversedIntStr = nStr[i] + reversedIntStr;
-  //   }
-  // }
-
-  // if (isNegative) reversedIntStr = '-' + reversedIntStr
-
-  // return Number(reversedIntStr);
-
-
 }
-
-console.log(reverseInt(51))
-console.log(reverseInt(981))
-console.log(reverseInt(500))
-console.log(reverseInt(-15))
-console.log(reverseInt(-90))
 
 module.exports = reverseInt;
